a1cdb654373a0bbf9b150b27d854dd674cee57de
Denis Radchenko <d.radchenko@itespecial.net>
2021-09-22 11:17:42 +0200

    upd: err problem

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,38 +1,44 @@
-import axios from 'axios'
+import axios from 'axios';
 import { AuthResponse } from '../models/response/RegResponse';
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 const $api = axios.create({
   // withCredentials: true,
-  baseURL: API_URL
-})
+  baseURL: API_URL,
+});
 
 $api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('access_token')
+  const token = localStorage.getItem('access_token');
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers.Authorization = `Bearer ${token}`;
   }
-  return config
-})
+  return config;
+});
 
-$api.interceptors.response.use((config) => {
-  return config
-}, (async (error) => {
-  if (error.response.status === 401) {
-    const originalRequest = error.config;
-    try {
-      const refresh = localStorage.getItem('refresh_token')
-      if (refresh) {
-        const response = await axios.post<AuthResponse>(`${process.env.REACT_APP_API_URL}/refresh`, {refresh_token: refresh});
-        localStorage.setItem('access_token', response.data?.access_token);
-        return $api.request(originalRequest)
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    if (error.response.status === 401) {
+      const originalRequest = error.config;
+      try {
+        const refresh = localStorage.getItem('refresh_token');
+        if (refresh) {
+          const response = await axios.post<AuthResponse>(
+            `${process.env.REACT_APP_API_URL}/refresh`,
+            { refresh_token: refresh }
+          );
+          localStorage.setItem('access_token', response.data?.access_token);
+          return $api.request(originalRequest);
+        }
+      } catch (e: any) {
+        console.warn('Пользователь не авторизован');
       }
-    } catch (e) {
-      console.warn('Пользователь не авторизован')
+      throw error;
     }
-    throw error;
   }
-}))
+);
 
-export default $api
+export default $api;
--- src/store/user/sagas.ts
+++ src/store/user/sagas.ts
@@ -10,14 +10,14 @@ import {
   loginUserSuccess,
   logoutUserSuccess,
   registerUserFailure,
-  registerUserSuccess
+  registerUserSuccess,
 } from './actions';
 import {
   CHECK_AUTH_USER_REQUEST,
   FETCH_USER_REQUEST,
   LOGIN_USER_REQUEST,
   LOGOUT_USER_REQUEST,
-  REGISTER_USER_REQUEST
+  REGISTER_USER_REQUEST,
 } from './actionTypes';
 import { IRefreshPayload, IUserLogin, IUserRegistration, RegisterUserRequest } from './types';
 import { Action } from 'redux-actions';
@@ -27,21 +27,23 @@ import $api from '../../http';
 
 const registrationUser = (payload: IUserRegistration): Promise<AxiosResponse<AuthResponse>> =>
   axios.post<AuthResponse>(`${process.env.REACT_APP_API_URL}/register`, {
-    ...payload
+    ...payload,
   });
 
 const loginUser = (payload: IUserLogin): Promise<AxiosResponse<AuthResponse>> =>
   $api.post<AuthResponse>(`${process.env.REACT_APP_API_URL}/login`, {
-    ...payload
+    ...payload,
   });
 const checkAuth = (payload: IRefreshPayload): Promise<AxiosResponse<AuthResponse>> =>
-  axios.post<AuthResponse>(`${process.env.REACT_APP_API_URL}/refresh`, {refresh_token: payload.refresh_token});
+  axios.post<AuthResponse>(`${process.env.REACT_APP_API_URL}/refresh`, {
+    refresh_token: payload.refresh_token,
+  });
 const fetchUser = (): Promise<AxiosResponse<AuthResponse>> =>
   $api.get<AuthResponse>(`${process.env.REACT_APP_API_URL}/user`, {
     headers: {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    }
+      Accept: 'application/json',
+    },
   });
 
 function* fetchUserSaga() {
@@ -55,9 +57,14 @@ function* fetchUserSaga() {
       })
     );
     if (!response.data?.data?.email_verified_at) {
-      yield put(modalsActions.openModalAction({name: 'mailConfirmModal', props: {text: response.data?.message}}))
+      yield put(
+        modalsActions.openModalAction({
+          name: 'mailConfirmModal',
+          props: { text: response.data?.message },
+        })
+      );
     }
-  } catch (e) {
+  } catch (e: any) {
     yield put(
       fetchUserFailure({
         errors: e.response.data.errors,
@@ -77,17 +84,22 @@ function* loginUserSaga(action: Action<RegisterUserRequest>) {
         api: {
           access_token: response.data?.access_token,
           refresh_token: response.data?.refresh_token,
-          expires_in: response.data?.expires_in
-        }
+          expires_in: response.data?.expires_in,
+        },
       })
     );
     localStorage.setItem('access_token', response.data?.access_token);
     localStorage.setItem('refresh_token', response.data?.refresh_token);
     localStorage.setItem('expires_in', response.data?.expires_in);
     if (!response.data?.data?.email_verified_at) {
-      yield put(modalsActions.openModalAction({name: 'mailConfirmModal', props: {text: response.data?.message}}))
+      yield put(
+        modalsActions.openModalAction({
+          name: 'mailConfirmModal',
+          props: { text: response.data?.message },
+        })
+      );
     }
-  } catch (e) {
+  } catch (e: any) {
     yield put(
       loginUserFailure({
         errors: e.response.data.errors || e.response.data.message,
@@ -102,7 +114,7 @@ function* registerUserSaga(action: Action<RegisterUserRequest>) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const response = yield call(registrationUser, action.payload);
-    console.log(response)
+    console.log(response);
     if (response?.data) {
       yield put(
         registerUserSuccess({
@@ -110,18 +122,23 @@ function* registerUserSaga(action: Action<RegisterUserRequest>) {
           api: {
             access_token: response.data?.access_token,
             refresh_token: response.data?.refresh_token,
-            expires_in: response.data?.expires_in
-          }
+            expires_in: response.data?.expires_in,
+          },
         })
       );
       localStorage.setItem('access_token', response.data?.access_token);
       localStorage.setItem('refresh_token', response.data?.refresh_token);
       localStorage.setItem('expires_in', response.data?.expires_in);
       if (!response.data?.data?.email_verified_at) {
-        yield put(modalsActions.openModalAction({name: 'mailConfirmModal', props: {text: response.data?.message}}))
+        yield put(
+          modalsActions.openModalAction({
+            name: 'mailConfirmModal',
+            props: { text: response.data?.message },
+          })
+        );
       }
     }
-  } catch (e) {
+  } catch (e: any) {
     console.log(e);
     yield put(
       registerUserFailure({
@@ -132,40 +149,37 @@ function* registerUserSaga(action: Action<RegisterUserRequest>) {
 }
 
 function* logoutUserSaga() {
-
   yield put(
     logoutUserSuccess({
       user: null,
-      api: null
+      api: null,
     })
   );
   localStorage.removeItem('access_token');
   localStorage.removeItem('refresh_token');
   localStorage.removeItem('expires_in');
-
 }
 
 function* checkAuthSaga() {
-
   const refreshToken = localStorage.getItem('refresh_token');
   try {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const response = yield call(checkAuth, {refresh_token: `${refreshToken}`});
+    const response = yield call(checkAuth, { refresh_token: `${refreshToken}` });
     yield put(
       checkAuthUserSuccess({
         user: response.data.data,
         api: {
           access_token: response.data?.access_token,
           refresh_token: response.data?.refresh_token,
-          expires_in: response.data?.expires_in
-        }
+          expires_in: response.data?.expires_in,
+        },
       })
     );
     localStorage.setItem('access_token', response.data?.access_token);
     localStorage.setItem('refresh_token', response.data?.refresh_token);
     localStorage.setItem('expires_in', response.data?.expires_in);
-  } catch (e) {
+  } catch (e: any) {
     yield put(
       checkAuthUserFailure({
         errors: e.response.data.errors,
@@ -173,7 +187,6 @@ function* checkAuthSaga() {
     );
   }
 }
-
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 function* userSaga(): any {
